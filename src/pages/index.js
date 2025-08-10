9a4622104da133b1ee37f64d4f26ad79d7ec8aa8
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2025-08-09 20:21:22 -0700

    added word posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -85,7 +85,7 @@ export default ({ data }) => {
     <Layout>
       <div>
         <h1>Yihua's Thoughts</h1>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
             <BlogLink to={node.fields.slug}>
